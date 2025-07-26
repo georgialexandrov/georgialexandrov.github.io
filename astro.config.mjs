9bdd92b95616fdb1d55bb365b031bb9532e79355
Georgi Alexandrov <georgi@alexandrov.dev>
2025-07-26 21:00:09 +0300

    Fix navigation active state for deployed site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://georgi.alexandrov.dev',
+  prefetch: false,
   markdown: {
     syntaxHighlight: 'prism',
   }
